1ccc3539dfe0ebba0a0704bd649f2271fbf4096b
Carlan Calazans <carlancalazans@gmail.com>
2020-02-08 22:24:08 +0000

    Client
    - Added gif of application screen
    - Updated lint config
    - Added error handling on network connection

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/jsx-filename-extension': 0	  
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0
   },
 };
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
-import ReactMapGL, { Popup, Marker, GeolocateControl, NavigationControl, FullscreenControl, ScaleControl } from 'react-map-gl';
+import ReactMapGL, {
+  Popup, Marker, GeolocateControl, NavigationControl, FullscreenControl, ScaleControl,
+} from 'react-map-gl';
 
-import './App.css'
+import './App.css';
 
 const App = () => {
   const [mapData, setMapData] = useState([]);
-  
+
   const [viewport, setViewport] = useState({
-    zoom: 1.9
+    zoom: 1.9,
   });
 
   const [selectedCountryRegion, setSelectedCountryRegion] = useState(null);
@@ -23,86 +25,118 @@ const App = () => {
 
   const listMapData = async () => {
     const response = await fetch('http://localhost:4000/map-data');
-    return response.json();
+
+    if (response.status >= 200 && response.status < 300) {
+      return response.json();
+    }
+
+    throw new Error(response.statusText);
   };
 
   useEffect(() => {
     (async () => {
-      const mapData = await listMapData();
-      setMapData(mapData);
+      try {
+        const data = await listMapData();
+        setMapData(data);
+        localStorage.setItem('mapData', JSON.stringify(data));
+      } catch (error) {
+        setMapData(JSON.parse(localStorage.getItem('mapData')));
+      }
     })();
   }, []);
 
   return (
     <ReactMapGL
       {...viewport}
-      width='100vw'
-      height='100vh'
+      width="100vw"
+      height="100vh"
       mapStyle="mapbox://styles/mapbox/dark-v9"
+      maxZoom={10}
       onViewportChange={setViewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}>
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+    >
 
-        <div style={{position: 'absolute', top: 0, left: 0, margin: 10}}>
-          <GeolocateControl
-            positionOptions={{enableHighAccuracy: true}}
-            trackUserLocation={true} />
-        </div>
+      <div style={{
+        position: 'absolute', top: 0, left: 0, margin: 10,
+      }}
+      >
+        <GeolocateControl
+          positionOptions={{ enableHighAccuracy: true, timeout: 30000 }}
+          trackUserLocation
+          fitBoundsOptions={{ maxZoom: 10 }}
+        />
+      </div>
 
-        <div style={{position: 'absolute', top: 36, left: 0, padding: '10px'}}>
-          <FullscreenControl />
-        </div>
+      <div style={{
+        position: 'absolute', top: 36, left: 0, padding: '10px',
+      }}
+      >
+        <FullscreenControl />
+      </div>
 
-        <div style={{position: 'absolute', top: 72, left: 0, padding: '10px'}}>
-          <NavigationControl />
-        </div>
+      <div style={{
+        position: 'absolute', top: 72, left: 0, padding: '10px',
+      }}
+      >
+        <NavigationControl />
+      </div>
 
-        <div style={{position: 'absolute', bottom: 72, left: 0, padding: '10px'}}>
-          <ScaleControl />
-        </div>
+      <div style={{
+        position: 'absolute', bottom: 72, left: 0, padding: '10px',
+      }}
+      >
+        <ScaleControl />
+      </div>
 
-        {
-          mapData.map((country, idx) => {
-            return (
-              <Marker key={idx} latitude={country.latlng[0]} longitude={country.latlng[1]} offsetTop={-20} offsetLeft={-10}>
-                <img src="pin.png" height="20" alt={country.countryTerritoryArea} style={{cursor: 'pointer'}} 
-                  onClick={() => {openPopup(country)}}/>
-              </Marker>
-            )
-          })
+      {
+          mapData.map((country, idx) => (
+            <Marker key={idx} latitude={country.latlng[0]} longitude={country.latlng[1]} offsetTop={-20} offsetLeft={-10}>
+              <img
+                src="pin.png"
+                height="20"
+                width="14"
+                alt={country.countryTerritoryArea}
+                style={{ cursor: 'pointer' }}
+                onClick={() => { openPopup(country); }}
+              />
+            </Marker>
+          ))
         }
 
-        {
-        selectedCountryRegion ? 
-          (
+      {
+        selectedCountryRegion
+          ? (
             <Popup
               latitude={selectedCountryRegion.latlng[0]}
               longitude={selectedCountryRegion.latlng[1]}
               onClose={closePopup}
               anchor="bottom"
               offsetLeft={8}
-              offsetTop={-8}>
-                <div>
-                  <h2>
-                    {selectedCountryRegion.countryTerritoryArea}
-                  </h2>
-                  <table class="country-region-statistics">
-                    <tbody>
-                      <tr>
-                        <th>Confirmed cases:</th>
-                        <td>{selectedCountryRegion.confirmedCases}</td>
-                      </tr>
-                      <tr>
-                        <th>Deaths:</th>
-                        <td>{selectedCountryRegion.deaths}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-            </Popup> 
-          ) : null}
+              offsetTop={-8}
+            >
+              <div>
+                <h2>
+                  {selectedCountryRegion.countryTerritoryArea}
+                </h2>
+                <table className="country-region-statistics">
+                  <tbody>
+                    <tr>
+                      <th>Confirmed cases:</th>
+                      <td>{selectedCountryRegion.confirmedCases}</td>
+                    </tr>
+                    <tr>
+                      <th>Deaths:</th>
+                      <td>{selectedCountryRegion.deaths}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </Popup>
+          ) : null
+}
 
     </ReactMapGL>
   );
-}
+};
 
 export default App;
